dfb3ed86a1039ad4d34752ee2b044a8302af1048
danielmtrdz <daniel_mata@outlook.com>
2018-10-30 17:26:52 -0600

    Arrays

---
+++ Proyecto/arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+	
+	//Crear arreglo de 10 posiciones y llenarlo usando los primeros
+	//10 numeros primos
+
+	int num [10] {2,3,5,7,13,17,19,23,29,31};
+	
+	
+	int main()
+	{
+	
+	
+	for (int i=0; i < 10; i++)
+ 	   {
+
+	   cout<< num[i] << endl;	  		   
+	   
+	   }
+	
+	return 0;
+
+	}
+
+
